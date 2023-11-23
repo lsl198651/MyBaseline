f831c92b6cd54d98d4474aed5b8da859a6cfeba4
lsl198651 <1986510281@qq.com>
2023-11-23 10:55:02 +0800

    save

--- CNN_run.py
+++ CNN_run.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     # parser.add_argument("--confusion_matrix_path", type=float,
     #                     default=1.0, help="ratio of absent and present",)
     parser.add_argument("--beta", type=float, default=(0.9, 0.98), help="beta")
-    parser.add_argument("--train_fold", type=list, default=['0', '1', '2', '4'])
+    parser.add_argument("--train_fold", type=list, default=['0', '1', '2', '3'])
     parser.add_argument("--test_fold", type=list, default=['4'])
     parser.add_argument("--setType", type=str, default=r"\06_new5fold")
     args = parser.parse_args()
